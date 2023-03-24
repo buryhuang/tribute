0eeb552b6cc06b9ccf0e6995076cd12a86f6f662
Bury Huang <bury.huang@MacBook-Pro-23.local>
2023-03-24 13:35:04 -0700

    update contract to intake list of artists, and image hash

--- contracts/arts.sol
+++ contracts/arts.sol
@@ -1,70 +1,118 @@
-// SPDX-License-Identifier: MIT
-
-// Smart Contract for AI Generated Art
-
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-interface IReferenceArtists {
-    function getReferenceArtists(bytes memory _image) external view returns (address[] memory, uint256[] memory);
-}
-
-contract AIGeneratedArt is ERC721 {
+contract AIArtFractionalNFT is ERC721 {
+    
+    struct Artist {
+        address artistAddress;
+        uint256 ownershipPercentage;
+    }
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
-    uint256 public fee;
-    address public referenceArtistsContract;
+    mapping(uint256 => Artist[]) private artistList;
+    mapping(uint256 => bytes32) private imageHashList;
+    mapping(address => uint256[]) private ownedTokens;
 
-    struct Art {
-        bytes image;
-        address[] referencedArtists;
-        uint256[] ownershipPercentages;
+    uint256 private feePercentage = 5;
+    address public contractOwner;
+
+    event ArtMinted(uint256 indexed tokenId, bytes32 imageHash, address[] artistAddresses, uint256[] ownershipPercentages);
+    event FeesDistributed(uint256 indexed tokenId, address[] artistAddresses, uint256[] distributedAmounts);
+
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+        contractOwner = msg.sender;
     }
 
-    mapping (uint256 => Art) public arts;
+    function mintAIArt(address[] memory artistAddresses, uint256[] memory ownershipPercentages, bytes32 imageHash) public payable {
+        require(artistAddresses.length > 0, "At least one referenced artist address is required");
+        require(artistAddresses.length == ownershipPercentages.length, "Artist addresses and ownership percentages length mismatch");
+        require(msg.value > 0, "Fee required to mint AIArt");
 
-    constructor(string memory _name, string memory _symbol, uint256 _fee, address _referenceArtistsContract) ERC721(_name, _symbol) {
-        fee = _fee;
-        referenceArtistsContract = _referenceArtistsContract;
-    }
-
-    function mint(bytes memory _image) external payable returns (uint256) {
-        require(msg.value >= fee, "Insufficient fee");
-        (address[] memory referencedArtists, uint256[] memory ownershipPercentages) = IReferenceArtists(referenceArtistsContract).getReferenceArtists(_image);
-        require(referencedArtists.length > 0, "No referenced artists found");
-        require(referencedArtists.length == ownershipPercentages.length, "Invalid ownership percentages");
+        // Calculate total ownership percentage
+        uint256 totalOwnershipPercentage;
+        for(uint256 i = 0; i < ownershipPercentages.length; i++) {
+            totalOwnershipPercentage += ownershipPercentages[i];
+        }
+        require(totalOwnershipPercentage == 100, "Total ownership percentage should be 100");
 
         uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
         _safeMint(msg.sender, tokenId);
-        arts[tokenId] = Art(_image, referencedArtists, ownershipPercentages);
+        _tokenIdCounter.increment();
 
-        uint256 distributedFees = 0;
-        for (uint256 i = 0; i < referencedArtists.length; i++) {
-            address artist = referencedArtists[i];
-            uint256 percentage = ownershipPercentages[i];
-            uint256 artistFee = (fee * percentage) / 100;
-            payable(artist).transfer(artistFee);
-            distributedFees += artistFee;
+        for(uint256 i = 0; i < artistAddresses.length; i++) {
+            artistList[tokenId].push(Artist(artistAddresses[i], ownershipPercentages[i]));
         }
-        // if (distributedFees < fee) {
-        //     payable(owner()).transfer(fee - distributedFees);
-        // }
+        imageHashList[tokenId] = imageHash;
 
-        return tokenId;
+        // Calculate and distribute fees
+        uint256 fees = msg.value * feePercentage / 100;
+        uint256 distributedFees;
+        uint256[] memory distributedAmounts = new uint256[](artistAddresses.length);
+        address[] memory distributedArtists = new address[](artistAddresses.length);
+
+        for(uint256 i = 0; i < artistAddresses.length; i++) {
+            uint256 artistFee = fees * ownershipPercentages[i] / 100;
+            payable(artistAddresses[i]).transfer(artistFee);
+            distributedFees += artistFee;
+            distributedAmounts[i] = artistFee;
+            distributedArtists[i] = artistAddresses[i];
+        }
+
+        payable(contractOwner).transfer(msg.value - distributedFees);
+
+        emit ArtMinted(tokenId, imageHash, artistAddresses, ownershipPercentages);
+        emit FeesDistributed(tokenId, distributedArtists, distributedAmounts);
     }
-}
 
-contract ReferencedArtist is ERC721 {
+    function getArtistList(uint256 tokenId) public view returns (address[] memory) {
+        Artist[] memory artists = artistList[tokenId];
+        address[] memory artistAddresses = new address[](artists.length);
 
-    string public artistName;
+        for(uint256 i = 0; i < artists.length; i++) {
+            artistAddresses[i] = artists[i].artistAddress;
+        }
 
-    constructor(string memory _artistName, string memory _symbol) ERC721(_artistName, _symbol) {
-        artistName = _artistName;
+        return artistAddresses;
+    }
+
+    function getOwnershipPercentageList(uint256 tokenId) public view returns (uint256[] memory) {
+        Artist[] memory artists = artistList[tokenId];
+        uint256[] memory ownershipPercentages = new uint256[](artists.length);
+
+        for(uint256 i = 0; i < artists.length; i++) {
+            ownershipPercentages[i] = artists[i].ownershipPercentage;
+        }
+
+        return ownershipPercentages;
+    }
+
+    function getImageHash(uint256 tokenId) public view returns (bytes32) {
+        return imageHashList[tokenId];
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize) internal virtual override {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+        _updateOwnedTokens(from, to, tokenId);
+    }
+
+    function _updateOwnedTokens(address from, address to, uint256 tokenId) internal virtual {
+        if(from != address(0)) {
+            uint256[] storage fromTokenList = ownedTokens[from];
+            for(uint256 i = 0; i < fromTokenList.length; i++) {
+                if(fromTokenList[i] == tokenId) {
+                    fromTokenList[i] = fromTokenList[fromTokenList.length - 1];
+                    fromTokenList.pop();
+                    break;
+                }
+            }
+        }
+
+        if(to != address(0)) {
+            ownedTokens[to].push(tokenId);
+        }
     }
 }
