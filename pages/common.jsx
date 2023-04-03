13e77069166468f1cb8246af2ac8ba349e9be701
Bury Huang <bury.huang@MacBook-Pro-23.local>
2023-04-03 11:01:09 -0700

    update app name

--- pages/common.jsx
+++ pages/common.jsx
@@ -4,7 +4,7 @@ export default function AboutContent() {
   return (
     <>
       <p>
-        Tribute is dedicated to giving due recognition and appreciation to the
+        Art-Honors is dedicated to giving due recognition and appreciation to the
         talented artists whose work has been featured in AIGC arts. Our goal is
         to ensure that these artists receive the acknowledgement and exposure
         they deserve, while also providing a comprehensive database for art
@@ -19,7 +19,7 @@ export default function AboutContent() {
       </p>
       <br />
       <p>
-        Through Tribute, we hope to raise awareness about the significance of
+        Through Art-Honors, we hope to raise awareness about the significance of
         acknowledging the creative contributions of artists, ultimately making a
         positive impact on the industry as a whole.
       </p>
