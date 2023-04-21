6d5182c9cabfec59b685a382d7e925c280ae9081
Bury Huang <bury.huang@MacBook-Pro-23.local>
2023-04-21 01:27:43 -0700

    switch to use private model

--- tribute-api/app.py
+++ tribute-api/app.py
@@ -22,31 +22,34 @@ def well_known(filename):
     return send_from_directory('static', filename)
 
 
-def process_image(image_bytes):
+def process_image(image_url):
     try:
-        # Generate sha256 hash of the image
-        image_hash = hashlib.sha256(image_bytes).hexdigest()
-
-        # Save the image to s3 bucket
-        s3.put_object(Body=image_bytes, Bucket='arthornors-images', Key=f"{image_hash}.png")
+        # # Generate sha256 hash of the image
+        # image_hash = hashlib.sha256(image_bytes).hexdigest()
+        #
+        # # Save the image to s3 bucket
+        # s3.put_object(Body=image_bytes, Bucket='arthornors-images', Key=f"{image_hash}.png")
 
         # Send the raw image bytes to Huggingface API
-        huggingface_response = requests.post("https://fffiloni-clip-interrogator-2.hf.space/run/clipi2", json={
-            "data": [
-                "data:image/png;base64," + b64encode(image_bytes).decode('utf-8'),
-                "best",
-                4,
-            ]
-        }).json()
+        # huggingface_response = requests.post("https://fffiloni-clip-interrogator-2.hf.space/run/clipi2", json={
+        #     "data": [
+        #         "data:image/png;base64," + b64encode(image_bytes).decode('utf-8'),
+        #         "best",
+        #         4,
+        #     ]
+        # }).json()
+        return requests.post("http://52.72.94.195:5000/process_image", json={
+            "image_url": image_url
+        }).json(), 200
 
-        # Extract the output data from Huggingface API response
-        output_data = huggingface_response.get('data')
-        if not output_data:
-            return jsonify({'error': 'no output data from Huggingface API'}), 500
-
-        print(output_data)
-        # Return the referenced artists
-        return jsonify({'data': get_referenced_artists(output_data[0])}), 200
+        # # Extract the output data from Huggingface API response
+        # output_data = huggingface_response.get('data')
+        # if not output_data:
+        #     return jsonify({'error': 'no output data from Huggingface API'}), 500
+        #
+        # print(output_data)
+        # # Return the referenced artists
+        # return jsonify({'data': get_referenced_artists(output_data[0])}), 200
 
     except requests.exceptions.HTTPError as e:
         return jsonify({'error': str(e)}), e.response.status_code
@@ -59,12 +62,12 @@ def process_image_api():
     if not image_url:
         return jsonify({'error': 'image_url not provided'}), 400
     try:
-        # Download image raw bytes from the provided image url
-        response = requests.get(image_url)
-        response.raise_for_status()
-        image_bytes = response.content
+        # # Download image raw bytes from the provided image url
+        # response = requests.get(image_url)
+        # response.raise_for_status()
+        # image_bytes = response.content
 
-        return process_image(image_bytes)
+        return process_image(image_url)
 
     except requests.exceptions.HTTPError as e:
         return jsonify({'error': str(e)}), e.response.status_code
@@ -77,11 +80,11 @@ def process_image_url_api():
         return jsonify({'error': 'image_url not provided'}), 400
     try:
         # Download image raw bytes from the provided image url
-        response = requests.get(image_url)
-        response.raise_for_status()
-        image_bytes = response.content
+        # response = requests.get(image_url)
+        # response.raise_for_status()
+        # image_bytes = response.content
 
-        return process_image(image_bytes)
+        return process_image(image_url)
 
     except requests.exceptions.HTTPError as e:
         return jsonify({'error': str(e)}), e.response.status_code
