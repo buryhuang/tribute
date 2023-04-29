746853f963082cba9af8b6e7b00c54e4d9cea22e
Bury Huang <bury.huang@MacBook-Pro-23.local>
2023-04-29 13:23:43 -0700

    cleanup, moving describe api to tribute api

--- tribute-api/app.py
+++ tribute-api/app.py
@@ -7,37 +7,82 @@ from dotenv import load_dotenv
 import boto3
 import hashlib
 from utils import get_referenced_artists
-import os
-import json
-from dotenv import dotenv_values
-import openai
+from PIL import Image
+import io 
+
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
 s3 = boto3.client('s3')
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
-@app.route('/.well-known/<path:filename>')
-def well_known(filename):
-    print(filename)
-    return send_from_directory('static', filename)
+import os, subprocess
+import torch
 
 
-def generate_image_with_prompt(prompt):
-    openai.api_key = OPENAI_API_KEY
+def setup():
+    install_cmds = [
+        ['pip', 'install', 'ftfy', 'gradio', 'regex', 'tqdm', 'transformers==4.21.2', 'timm', 'fairscale', 'requests'],
+        ['pip', 'install', 'open_clip_torch'],
+        ['pip', 'install', '-e', 'git+https://github.com/pharmapsychotic/BLIP.git@lib#egg=blip'],
+        ['git', 'clone', '-b', 'open-clip', 'https://github.com/pharmapsychotic/clip-interrogator.git']
+    ]
+    for cmd in install_cmds:
+        print(subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode('utf-8'))
 
-    response = openai.Image.create(
-        prompt=prompt,
-        n=1,
-        size="1024x1024"
-    )
-    image_url = response['data'][0]['url']
-    return image_url
+
+setup()
+
+# download cache files
+print("Download preprocessed cache files...")
+CACHE_URLS = [
+    'https://huggingface.co/pharma/ci-preprocess/resolve/main/ViT-H-14_laion2b_s32b_b79k_artists.pkl',
+    'https://huggingface.co/pharma/ci-preprocess/resolve/main/ViT-H-14_laion2b_s32b_b79k_flavors.pkl',
+    'https://huggingface.co/pharma/ci-preprocess/resolve/main/ViT-H-14_laion2b_s32b_b79k_mediums.pkl',
+    'https://huggingface.co/pharma/ci-preprocess/resolve/main/ViT-H-14_laion2b_s32b_b79k_movements.pkl',
+    'https://huggingface.co/pharma/ci-preprocess/resolve/main/ViT-H-14_laion2b_s32b_b79k_trendings.pkl',
+]
+os.makedirs('cache', exist_ok=True)
+for url in CACHE_URLS:
+    print(subprocess.run(['wget', url, '-P', 'cache'], stdout=subprocess.PIPE).stdout.decode('utf-8'))
+
+import sys
+
+sys.path.append('src/blip')
+sys.path.append('clip-interrogator')
+
+import gradio as gr
+from clip_interrogator import Config, Interrogator
+
+config = Config(clip_model_name="ViT-H-14/laion2b_s32b_b79k")
+config.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+config.blip_offload = False if torch.cuda.is_available() else True
+config.chunk_size = 2048
+config.flavor_intermediate_count = 512
+config.blip_num_beams = 64
+
+ci = Interrogator(config)
 
 
-def process_image(image_url):
+def inference(image_bytes, mode, best_max_flavors):
+    stream = io.BytesIO(image_bytes)
+    image = Image.open(stream).convert('RGB')  # open image with PIL and convert to RGB
+    if mode == 'best':
+        prompt_result = ci.interrogate(image, max_flavors=int(best_max_flavors))
+        print("mode best: " + prompt_result)
+        return prompt_result, gr.update(visible=True), gr.update(visible=True), gr.update(visible=True)
+    elif mode == 'classic':
+        prompt_result = ci.interrogate_classic(image)
+        print("mode classic: " + prompt_result)
+        return prompt_result, gr.update(visible=True), gr.update(visible=True), gr.update(visible=True)
+    else:
+        prompt_result = ci.interrogate_fast(image)
+        print("mode fast: " + prompt_result)
+        return prompt_result, gr.update(visible=True), gr.update(visible=True), gr.update(visible=True)
+
+def process_image(image_bytes, image_url):
     try:
         # # Generate sha256 hash of the image
         # image_hash = hashlib.sha256(image_bytes).hexdigest()
@@ -46,32 +91,27 @@ def process_image(image_url):
         # s3.put_object(Body=image_bytes, Bucket='arthornors-images', Key=f"{image_hash}.png")
 
         # Send the raw image bytes to Huggingface API
-        # huggingface_response = requests.post("https://fffiloni-clip-interrogator-2.hf.space/run/clipi2", json={
-        #     "data": [
-        #         "data:image/png;base64," + b64encode(image_bytes).decode('utf-8'),
-        #         "best",
-        #         4,
-        #     ]
-        # }).json()
-        describe_result = requests.post("http://52.72.94.195:5000/process_image", json={
-            "image_url": image_url
-        }).json()
-        # print(describe_result)
+        prompt, gr1, gr2, gr3  = inference(image_bytes, 'classic', 4)
 
-        # generated_url = generate_image_with_prompt(describe_result['data']['prompt'])
-        # print(generated_url)
-
-        # describe_result['data']['similar_image'] = generated_url
-        return jsonify(describe_result), 200
-
-        # # Extract the output data from Huggingface API response
-        # output_data = huggingface_response.get('data')
-        # if not output_data:
-        #     return jsonify({'error': 'no output data from Huggingface API'}), 500
-        #
-        # print(output_data)
-        # # Return the referenced artists
-        # return jsonify({'data': get_referenced_artists(output_data[0])}), 200
+        print(prompt, gr1, gr2, gr3)
+        # Return the referenced artists
+        return jsonify({
+            'data': {
+                'artists': get_referenced_artists(prompt),
+                'possible_prompt': prompt,
+                'possible_sources': [
+                    {
+                        "type": "web",
+                        "data": {
+                            "source": "web",
+                            "chain": "",
+                            "offchain_address": image_url,
+                            "onchain_address": ""
+                        }
+                    }
+                ]
+            }
+        }), 200
 
     except requests.exceptions.HTTPError as e:
         return jsonify({'error': str(e)}), e.response.status_code
@@ -84,12 +124,12 @@ def process_image_api():
     if not image_url:
         return jsonify({'error': 'image_url not provided'}), 400
     try:
-        # # Download image raw bytes from the provided image url
-        # response = requests.get(image_url)
-        # response.raise_for_status()
-        # image_bytes = response.content
+        # Download image raw bytes from the provided image url
+        response = requests.get(image_url)
+        response.raise_for_status()
+        image_bytes = response.content
 
-        return process_image(image_url)
+        return process_image(image_bytes, image_url)
 
     except requests.exceptions.HTTPError as e:
         return jsonify({'error': str(e)}), e.response.status_code
@@ -102,11 +142,11 @@ def process_image_url_api():
         return jsonify({'error': 'image_url not provided'}), 400
     try:
         # Download image raw bytes from the provided image url
-        # response = requests.get(image_url)
-        # response.raise_for_status()
-        # image_bytes = response.content
+        response = requests.get(image_url)
+        response.raise_for_status()
+        image_bytes = response.content
 
-        return process_image(image_url)
+        return process_image(image_bytes, image_url)
 
     except requests.exceptions.HTTPError as e:
         return jsonify({'error': str(e)}), e.response.status_code
