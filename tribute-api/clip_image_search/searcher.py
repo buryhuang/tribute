6822cafa0e15f57e8f4681bf73cd25f83378ab45
Bury Huang <bury.huang@MacBook-Pro-23.local>
2023-04-29 16:18:56 -0700

    switch to use basic es endpoint

--- tribute-api/clip_image_search/searcher.py
+++ tribute-api/clip_image_search/searcher.py
@@ -4,17 +4,41 @@ from elasticsearch.helpers import bulk
 from dotenv import load_dotenv
 import os
 
+def send_to_elasticsearch(endpoint, username, password):
+    # create an authorization handler
+    p = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+    p.add_password(None, endpoint, username, password);
+
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+    auth_handler = urllib.request.HTTPBasicAuthHandler(p)
+    https_handler = urllib.request.HTTPSHandler(context=ctx)
+    opener = urllib.request.build_opener(https_handler, auth_handler)
+
+    urllib.request.install_opener(opener)
+
+    try:
+        req = urllib.request.Request(top_level_url, data=data.encode('utf-8'),
+                                     headers={'Content-Type': 'application/json'}
+                                     )
+        result = opener.open(req)
+        messages = result.read()
+        # print(messages)
+    except IOError as e:
+        print(e)
+
 
 class Searcher:
     def __init__(self, region="us-east-1"):
         load_dotenv()
-        es_endpoint = os.environ.get("ES_ENDPOINT")
-        es_username = os.environ.get("ES_USER")
-        es_password = os.environ.get("ES_PASSWORD")
+        self.es_endpoint = os.environ.get("ES_ENDPOINT")
+        self.es_username = os.environ.get("ES_USER")
+        self.es_password = os.environ.get("ES_PASSWORD")
 
         self.client = Elasticsearch(
-            hosts=[es_endpoint],
-            http_auth=(es_username, es_password),
+            hosts=[self.es_endpoint],
+            http_auth=(self.es_username, self.es_password),
             headers={"Content-Type": "application/json"},
             # use_ssl=True,
             # verify_certs=True,
@@ -37,7 +61,8 @@ class Searcher:
                 }
             },
         }
-        return self.client.indices.create(index=self.index_name, body=knn_index, ignore=400)
+        # return self.client.indices.create(index=self.index_name, body=knn_index, ignore=400)
+        send_to_elasticsearch(f"{self.es_endpoint}/{self.index_name}", self.es_username, self.es_password)
 
     def bulk_ingest(self, generate_data, chunk_size=128):
         return bulk(self.client, generate_data, chunk_size=chunk_size)
